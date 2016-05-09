02d51704a2627773f93a15de246219564e313ad6
Andrew Isidoro <andrew@seofosho.com>
2016-05-09 22:48:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -49,26 +49,7 @@ app.post('/webhook/', function (req, res) {
 
 var token = "EAAP1vk86NaQBANoBaSt6cYq3H7ZCZCwWEbgcKoVLSuQFiCqRZBXRtgOCegZBfHtTFSAgj4ZCH6xtNBP0Wa7JPZANlJvjC5vPbkVBbAXJVKMLhacNZAKrqcbINbEPXIZAEGFJZBDZCD3MFxGOqlizQfuZARfZAXj9EQITTg66kfQgjlXI1gZDZD"
 
-function sendTextMessage(sender, text) {
-	messageData = {
-		text:text
-	}
-	request({
-		url: 'https://graph.facebook.com/v2.6/me/messages',
-		qs: {access_token:token},
-		method: 'POST',
-		json: {
-			recipient: {id:sender},
-			message: messageData,
-		}
-	}, function(error, response, body) {
-		if (error) {
-			console.log('Error sending messages: ', error)
-		} else if (response.body.error) {
-			console.log('Error: ', response.body.error)
-		}
-	})
-}
+function sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
 
 function sendGenericMessage(sender) {
 	messageData = {
